8c70df3606bf14bc37a5edc49baf1ed63985c6cc
Dmytro Mytysh <mitishdima@yandex.ua>
2022-07-17 19:44:59 +0200

    initial new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { WorksComponent } from './components/works/works.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WorksComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
